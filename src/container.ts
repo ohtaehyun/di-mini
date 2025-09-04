386b12a8916201a99bd441056e22a583738ffc87
ohtaehyun <othdev95@gmail.com>
2025-09-05 01:00:20 +0900

    feat: resolve 함수 구현

--- src/container.ts
+++ src/container.ts
@@ -9,6 +9,30 @@ export class Container {
   }
 
   public register(key: string, value: any): void {
+    if (Container.dependencies.has(key)) {
+      throw new Error(`Dependency with key ${key} is already registered.`);
+    }
+
     Container.dependencies.set(key, value);
+  }
+
+  public resolve<T>(key: string): T {
+    const target = Container.dependencies.get(key);
+
+    if (!target) {
+      throw new Error(`No dependency found for key: ${key}`);
+    }
+
+    const paramTypes: any[] = Reflect.getMetadata("design:paramtypes", target) || [];
+
+    const injections = paramTypes.map((_: any, index: number) => {
+      const injectKey = Reflect.getMetadata(`inject:${index}`, target);
+      if (!injectKey) {
+        throw new Error(`No injection metadata found for parameter at index ${index} of ${key}`);
+      }
+      return this.resolve(injectKey);
+    });
+
+    return new target(...injections);
   }
 }
