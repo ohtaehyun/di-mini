3aa99d61e290d50f73084ed95d10d7854556b82d
ohtaehyun <othdev95@gmail.com>
2025-09-05 00:06:55 +0900

    feat: 의존성을 저장할 dependency 추가

--- src/container.ts
+++ src/container.ts
@@ -1,9 +1,14 @@
 export class Container {
   private static instance: Container;
+  private static dependencies = new Map<string, any>();
 
   private constructor() {}
 
   public static getInstance(): Container {
     return Container.instance ?? (Container.instance = new Container());
+  }
+
+  public register(key: string, value: any): void {
+    Container.dependencies.set(key, value);
   }
 }
