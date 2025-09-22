47c0dff15f0c89447dbb33459ab193cc693c19a5
ohtaehyun <othdev95@gmail.com>
2025-09-22 15:28:18 +0900

    refactor: pathVo를 사용해서 컨트롤러 데코레이터 수정

--- src/decorators/controller.ts
+++ src/decorators/controller.ts
@@ -1,9 +1,10 @@
 import { Injectable } from "./injectable.js";
 import { ControllerRegistry } from "../registries/index.js";
+import { PathVO } from "../models/path.vo.js";
 
 export function Controller(path: string): ClassDecorator {
   return function (target: any) {
     Injectable()(target);
-    ControllerRegistry.register(path, target);
+    ControllerRegistry.register(new PathVO(path).toString(), target);
   };
 }
